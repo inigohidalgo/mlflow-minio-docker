92f4a504e27e916f05d1114f7468374f5202a245
inigohidalgo <inigohrey@gmail.com>
2024-01-28 15:09:01 +0100

    fix: ocker to docker

--- tasks/docker-start.sh
+++ tasks/docker-start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ocker compose --env-file docker/config.env -f docker/docker-compose.yml up -d --build
+docker compose --env-file docker/config.env -f docker/docker-compose.yml up -d --build
